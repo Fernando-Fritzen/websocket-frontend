21346ee523f7f110b9e51c812346f9598ddccd0a
Fernando Fritzen <fernandofritzen9@gmail.com>
2022-06-10 20:23:56 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react'
 import './App.css';
+import { MapContainer, TileLayer } from 'react-leaflet'
+import osm from './osm-providers'
 
 function App() {
+
+  const [center, setCenter] = useState({ lat: 13.084, lng: 80.2449 })
+  const ZOOM_LEVEL = 9;
+  const mapRef = useRef();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ola</h1>
+      <MapContainer
+        center={center}
+        zoom={ZOOM_LEVEL}
+        ref={mapRef}
+      >
+        <TileLayer url={osm.maptiler.url} attribution={osm.maptiler.attribution} />
+      </MapContainer>
     </div>
   );
 }
