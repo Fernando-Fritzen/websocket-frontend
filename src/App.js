8376e893c7b5dc9534aee53497fe8f5b683b0db8
Fernando F Pieske <fernando.pieske@projedata.com.br>
2022-06-20 23:37:50 -0300

    Finished connection

--- src/App.js
+++ src/App.js
@@ -1,102 +1,122 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { useMapEvents } from "react-leaflet";
-import osm from "./osm-providers";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
-import io from 'socket.io-client'
-const endpoint = "http://192.168.0.104:3001"
-const socket = io.connect("http://localhost:3001")
+import io from "socket.io-client";
+const socket = io.connect("http://192.168.0.104:3001");
 
 function App() {
-
-  
-  useEffect(() => {
-    socket.on('listaMarca', (data) => {
-      // setListOfMarkers(data)
-      alert("Recebido")
-      console.log('Recebido...')
-    })
-  }, [socket])
+  const [listOfMarkers, setListOfMarkers] = useState([]);
+  const [messageValue, setMessageValue] = useState("");
+  const [currentPosition, setCurrentPosition] = useState();
+  const [isOnline, setNetwork] = useState(window.navigator.onLine);
+  const [connectionLost, setConnectionLost] = useState(false)
 
   useEffect(() => {
-    // const socket = io(endpoint);
-
-    socket.emit('ffff', {fe: "fefe"})
-    console.log("Lista: ",listOfMarkers)
-  }, [])
-
-  const [center, setCenter] = useState({ lat: -25.284, lng: 305.2449 });
-  const ZOOM_LEVEL = 9;
-  const mapRef = useRef();
-  const [listOfMarkers, setListOfMarkers] = useState([
-    {text: "teste", position: {lat: -25.54755254134766, lng: -54.54746246337891}},
-    {text: "teste2", position: {lat: -25.571091631069365, lng: -54.57080841064454}}
-  ])
-  const [buttonValue, setButtonValue] = useState("")
+    socket.on("allMarkers", (data) => {
+      setListOfMarkers(data);
+    });
+  }, [socket]);
 
   const markerIcon = new L.Icon({
-    iconUrl: require("./placeholder.png"),
-    iconSize: [35, 45],
+    iconUrl: require("./assets/placeholder.png"),
+    iconSize: [35, 35],
     iconAnchor: [17, 46],
     popupAnchor: [3, -46],
   });
 
-  const [currentPosition, setCurrentPosition] = useState({lat: -25.535006795752302, lng: -54.60273742675782})
   function LocationMarker() {
     const map = useMapEvents({
       click(e) {
-        socket.emit("ffff", {})
-        console.log(typeof(e.latlng.lat))
-        setCurrentPosition(e.latlng)
-        console.log("Evento: ",e.latlng)
-      }
-    })
+        setCurrentPosition(e.latlng);
+      },
+    });
   }
 
+  // function IsOnline() {
+  //   return window.navigator.onLine;
+  // }
+
+  
   useEffect(() => {
-    
-  }, listOfMarkers)
+    window.addEventListener("offline", setNetwork(window.navigator.onLine));
+    window.addEventListener("online", setNetwork(window.navigator.onLine));
+    return () => {
+      window.removeEventListener("offline", setNetwork(window.navigator.onLine));
+      window.removeEventListener("online", setNetwork(window.navigator.onLine));
+    };
+  });
+  
+  const sendLocalStorageData = async () => {
+    console.log('Sincronizando......')
+    const storageMarkers = localStorage.getItem("storageMarkers");
 
-  function saveMarker() {
-    const newMarker = {text: buttonValue, position: currentPosition}
-    const list = listOfMarkers
-    list.push(newMarker)
-    setListOfMarkers(list)
-    console.log("Lista: ",listOfMarkers)
+    if(storageMarkers) {
+      const storageMarkersJson = JSON.parse(storageMarkers);
+
+      storageMarkersJson.map(marker => socket.emit('saveMarker', marker))
+      localStorage.removeItem('storageMarkers')
+    }
   }
+
+  const saveMarker = async () => {
+    const { lat, lng } = currentPosition;
+    const newMarker = { mensagem: messageValue, lat, lng };
+    if (isOnline) {
+      await socket.emit("saveMarker", newMarker);
+      setCurrentPosition();
+    } else {
+      setConnectionLost(true)
+      const markers = [];
+      const storageMarkers = localStorage.getItem("storageMarkers");
+
+      if (storageMarkers) {
+        const storageMarkersJson = JSON.parse(storageMarkers);
+        storageMarkersJson.map((marker) => markers.push(marker));
+      }
+
+      markers.push(newMarker);
+      localStorage.setItem("storageMarkers", JSON.stringify(markers));
+      setListOfMarkers([...listOfMarkers, newMarker]);
+    }
+  };
+
+  useEffect(()=>{
+    if(connectionLost && isOnline) {
+      setConnectionLost(false)
+      sendLocalStorageData()
+    }
+  },[isOnline, connectionLost])
 
   return (
     <div className="App">
-      {/* <MapContainer
-        center={center}
-        zoom={ZOOM_LEVEL}
-        ref={mapRef}
+      <MapContainer
+        center={{ lat: -25.553399029829066, lng: -54.56973552703858 }}
+        zoom={13}
+        scrollWheelZoom={true}
       >
-        <TileLayer url={osm.maptiler.url} attribution={osm.maptiler.attribution} />
-        <Marker position={[-25.284, 305.2449]} icon={markerIcon}>
-          <Popup>
-            <p>Aqui é Foz do Iguaçú!!!!!</p>
-          </Popup>
-        </Marker>
-      </MapContainer> */}
-      <MapContainer center={{lat: -25.553399029829066, lng: -54.56973552703858}} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={currentPosition} icon={markerIcon}>
-          <Popup>
-            <input type="text" value={buttonValue} onChange={e => setButtonValue(e.target.value)} />
-            <button onClick={() => saveMarker()}>Salvar</button>
-          </Popup>
-        </Marker>
-        {listOfMarkers?.map(marker => (
-          <Marker position={marker.position} icon={markerIcon}>
+
+        {currentPosition ? (
+          <Marker position={currentPosition} icon={markerIcon}>
             <Popup>
-              {marker.text}
+              <input
+                type="text"
+                value={messageValue}
+                onChange={(e) => setMessageValue(e.target.value)}
+              />
+              <button onClick={() => saveMarker()}>Salvar</button>
             </Popup>
+          </Marker>
+        ) : null}
+        {listOfMarkers?.map((marker) => (
+          <Marker position={[marker.lat, marker.lng]} icon={markerIcon}>
+            <Popup>{marker.mensagem}</Popup>
           </Marker>
         ))}
         <LocationMarker />
--- src/osm-providers.js
+++
@@ -1,6 +0,0 @@
-export default {
-    maptiler: {
-        url: "https://api.maptiler.com/maps/basic/{z}/{x}/{y}.png?key=l5rTwIrT8yqjSUbMi271",
-        attribution: "https://api.maptiler.com/maps/basic/tiles.json?key=l5rTwIrT8yqjSUbMi271"
-    }
-}
